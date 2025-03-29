e841e1eebad07cff31832e407415f9086ca3e084
yuzai <a545905403@163.com>
2025-03-29 15:24:48 +0800

    fix: path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/imagesize/', // 将此处替换为你的GitHub仓库名
+  base: process.env.VITE_BASE_PATH || '/',  // 使用环境变量或默认为根路径
 })
